27a3208d6ad3a5ad725d1da9096f6882a6293462
Awrash <arashmardan.am@gmail.com>
2017-11-30 04:39:18 -0800

    Update bot.lua
    updated Sudo ID

--- bot/bot.lua
+++ bot/bot.lua
@@ -302,7 +302,7 @@ function create_config()
     autoleave = false,
     bot_api = {
       key = bot_api_key,
-      master = our_id,
+      master = our_jd,
       uid = botid.id,
       uname = botid.username
     },
@@ -340,7 +340,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {[our_id] = our_id}
+    sudo_users = {474876787}
   }
   save_config()
 end
